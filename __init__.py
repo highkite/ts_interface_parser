9c0f7af0178bafcf6ae49db9f6a2511a11496f63
Thomas Osterland <thomas.osterland@fit.fraunhofer.de>
2019-10-15 11:49:28 +0200

    prepared pypi packaging

---
+++ __init__.py
@@ -0,0 +1,1 @@
+name = "ts_interface_parser"
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ts_interface_parser",
+    version="0.0.1",
+    author="Thomas Osterland",
+    author_email="highway.ita07@web.de",
+    description="The typescript interface parser parses interfaces defined in typescript and outputs a JSON object describing the interfaces.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
